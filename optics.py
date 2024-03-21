9ac6472ead69aea9cbc1aa52750d503af2eff58f
louis <louis.chevallier@gmail.com>
2024-03-21 16:29:01 +0100

    v1

--- optics.py
+++ optics.py
@@ -24,3 +24,17 @@ s2 = solve([eq1, eq2, eq3, eq4, eq5], [ai, bi, i, aih, bih])
 EKOX(len(s2))
 EKOX(s2)
 EKOX('\n'.join(map(str, s2[0])))
+
+
+
+veq = [ fb - 20, bih - 600, h-100, i-3000]
+eee = solve([eq1, eq2, eq3, eq4, eq5] + veq, [ a, b])
+EKOX(eee)
+
+
+fff = solve([eq1, fa-50, ai-100], [ a])
+EKOX(fff)
+
+
+
+EKOX(eee)
